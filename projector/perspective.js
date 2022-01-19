c89c1e86f3f86a0a4725bd5c38032a4c18322e46
Montana Mendy <montana@linux.com>
2022-01-19 03:38:29 -0800

    Update perspective.js

--- projector/perspective.js
+++ projector/perspective.js
@@ -16,21 +16,25 @@
 // 2010-08-28
 
 /* -------------------------------------------------------------------
-* define objects (name space) for this library.
-* ----------------------------------------------------------------- */
-if( typeof html5jp == 'undefined' ) {
+ * define objects (name space) for this library.
+ * ----------------------------------------------------------------- */
+if (typeof html5jp == 'undefined') {
     html5jp = new Object();
 }
 
-(function () {
+(function() {
 
     /* -------------------------------------------------------------------
-    * constructor
-    * ----------------------------------------------------------------- */
+     * constructor
+     * ----------------------------------------------------------------- */
     html5jp.perspective = function(ctxd, image) {
         // check the arguments
-        if( ! ctxd || ! ctxd.strokeStyle ) { return; }
-        if( ! image || ! image.width || ! image.height ) { return; }
+        if (!ctxd || !ctxd.strokeStyle) {
+            return;
+        }
+        if (!image || !image.width || !image.height) {
+            return;
+        }
         // prepare a <canvas> for the image
         var cvso = document.createElement('canvas');
         cvso.width = parseInt(image.width);
@@ -52,13 +56,13 @@ if( typeof html5jp == 'undefined' ) {
     };
 
     /* -------------------------------------------------------------------
-    * prototypes
-    * ----------------------------------------------------------------- */
+     * prototypes
+     * ----------------------------------------------------------------- */
     var proto = html5jp.perspective.prototype;
 
     /* -------------------------------------------------------------------
-    * public methods
-    * ----------------------------------------------------------------- */
+     * public methods
+     * ----------------------------------------------------------------- */
     proto.draw = function(points) {
         var d0x = points[0][0];
         var d0y = points[0][1];
@@ -70,10 +74,10 @@ if( typeof html5jp == 'undefined' ) {
         var d3y = points[3][1];
         // compute the dimension of each side
         var dims = [
-            Math.sqrt( Math.pow(d0x-d1x, 2) + Math.pow(d0y-d1y, 2) ), // top side
-            Math.sqrt( Math.pow(d1x-d2x, 2) + Math.pow(d1y-d2y, 2) ), // right side
-            Math.sqrt( Math.pow(d2x-d3x, 2) + Math.pow(d2y-d3y, 2) ), // bottom side
-            Math.sqrt( Math.pow(d3x-d0x, 2) + Math.pow(d3y-d0y, 2) )  // left side
+            Math.sqrt(Math.pow(d0x - d1x, 2) + Math.pow(d0y - d1y, 2)), // top side
+            Math.sqrt(Math.pow(d1x - d2x, 2) + Math.pow(d1y - d2y, 2)), // right side
+            Math.sqrt(Math.pow(d2x - d3x, 2) + Math.pow(d2y - d3y, 2)), // bottom side
+            Math.sqrt(Math.pow(d3x - d0x, 2) + Math.pow(d3y - d0y, 2)) // left side
         ];
         //
         var ow = this.p.cvso.width;
@@ -82,22 +86,24 @@ if( typeof html5jp == 'undefined' ) {
         var base_index = 0;
         var max_scale_rate = 0;
         var zero_num = 0;
-        for( var i=0; i<4; i++ ) {
+        for (var i = 0; i < 4; i++) {
             var rate = 0;
-            if( i % 2 ) {
+            if (i % 2) {
                 rate = dims[i] / ow;
             } else {
                 rate = dims[i] / oh;
             }
-            if( rate > max_scale_rate ) {
+            if (rate > max_scale_rate) {
                 base_index = i;
                 max_scale_rate = rate;
             }
-            if( dims[i] == 0 ) {
-                zero_num ++;
+            if (dims[i] == 0) {
+                zero_num++;
             }
         }
-        if(zero_num > 1) { return; }
+        if (zero_num > 1) {
+            return;
+        }
         //
         var step = 2;
         var cover_step = step * 5;
@@ -105,17 +111,17 @@ if( typeof html5jp == 'undefined' ) {
         var ctxo = this.p.ctxo;
         var ctxt = this.p.ctxt;
         ctxt.clearRect(0, 0, ctxt.canvas.width, ctxt.canvas.height);
-        if(base_index % 2 == 0) { // top or bottom side
+        if (base_index % 2 == 0) { // top or bottom side
             var ctxl = this.create_canvas_context(ow, cover_step);
             var cvsl = ctxl.canvas;
-            for( var y=0; y<oh; y+=step ) {
+            for (var y = 0; y < oh; y += step) {
                 var r = y / oh;
-                var sx = d0x + (d3x-d0x) * r;
-                var sy = d0y + (d3y-d0y) * r;
-                var ex = d1x + (d2x-d1x) * r;
-                var ey = d1y + (d2y-d1y) * r;
-                var ag = Math.atan( (ey-sy) / (ex-sx) );
-                var sc = Math.sqrt( Math.pow(ex-sx, 2) + Math.pow(ey-sy, 2) ) / ow;
+                var sx = d0x + (d3x - d0x) * r;
+                var sy = d0y + (d3y - d0y) * r;
+                var ex = d1x + (d2x - d1x) * r;
+                var ey = d1y + (d2y - d1y) * r;
+                var ag = Math.atan((ey - sy) / (ex - sx));
+                var sc = Math.sqrt(Math.pow(ex - sx, 2) + Math.pow(ey - sy, 2)) / ow;
                 ctxl.setTransform(1, 0, 0, 1, 0, -y);
                 ctxl.drawImage(ctxo.canvas, 0, 0);
                 //
@@ -126,17 +132,17 @@ if( typeof html5jp == 'undefined' ) {
                 //
                 ctxt.setTransform(1, 0, 0, 1, 0, 0);
             }
-        } else if(base_index % 2 == 1) { // right or left side
+        } else if (base_index % 2 == 1) { // right or left side
             var ctxl = this.create_canvas_context(cover_step, oh);
             var cvsl = ctxl.canvas;
-            for( var x=0; x<ow; x+=step ) {
-                var r =  x / ow;
-                var sx = d0x + (d1x-d0x) * r;
-                var sy = d0y + (d1y-d0y) * r;
-                var ex = d3x + (d2x-d3x) * r;
-                var ey = d3y + (d2y-d3y) * r;
-                var ag = Math.atan( (sx-ex) / (ey-sy) );
-                var sc = Math.sqrt( Math.pow(ex-sx, 2) + Math.pow(ey-sy, 2) ) / oh;
+            for (var x = 0; x < ow; x += step) {
+                var r = x / ow;
+                var sx = d0x + (d1x - d0x) * r;
+                var sy = d0y + (d1y - d0y) * r;
+                var ex = d3x + (d2x - d3x) * r;
+                var ey = d3y + (d2y - d3y) * r;
+                var ag = Math.atan((sx - ex) / (ey - sy));
+                var sc = Math.sqrt(Math.pow(ex - sx, 2) + Math.pow(ey - sy, 2)) / oh;
                 ctxl.setTransform(1, 0, 0, 1, -x, 0);
                 ctxl.drawImage(ctxo.canvas, 0, 0);
                 //
@@ -150,14 +156,19 @@ if( typeof html5jp == 'undefined' ) {
         }
         // set a clipping path and draw the transformed image on the destination canvas.
         this.p.ctxd.save();
-        this.set_clipping_path(this.p.ctxd, [[d0x, d0y], [d1x, d1y], [d2x, d2y], [d3x, d3y]]);
+        this.set_clipping_path(this.p.ctxd, [
+            [d0x, d0y],
+            [d1x, d1y],
+            [d2x, d2y],
+            [d3x, d3y]
+        ]);
         this.p.ctxd.drawImage(ctxt.canvas, 0, 0);
         this.p.ctxd.restore();
     }
 
     /* -------------------------------------------------------------------
-    * private methods
-    * ----------------------------------------------------------------- */
+     * private methods
+     * ----------------------------------------------------------------- */
 
     proto.create_canvas_context = function(w, h) {
         var canvas = document.createElement('canvas');
@@ -170,11 +181,11 @@ if( typeof html5jp == 'undefined' ) {
     proto.set_clipping_path = function(ctx, points) {
         ctx.beginPath();
         ctx.moveTo(points[0][0], points[0][1]);
-        for( var i=1; i<points.length; i++ ) {
+        for (var i = 1; i < points.length; i++) {
             ctx.lineTo(points[i][0], points[i][1]);
         }
         ctx.closePath();
         ctx.clip();
     };
 
-})();
+})();%       
